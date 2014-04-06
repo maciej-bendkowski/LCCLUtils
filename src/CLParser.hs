51fba4183ada373799a90a187f3d48897337ae3d
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2014-04-06 14:29:11 +0200

    Recursive descent parser utilities along with CL parser

---
+++ src/CLParser.hs
@@ -0,0 +1,71 @@
+{-|
+    Copyright (c) 2014 Maciej Bendkowski
+  
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+    
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-}
+module CLParser where
+    import ParserUtils
+    import CL
+    
+    -- CL grammar
+    -- <combinator> := "S" | "K"
+    -- <subterm> := <combinator> | "(" <term> ")"
+    -- <term> := <subterm>+
+    
+    -- combinator parser
+    combinatorParser :: Parser Term
+    combinatorParser = sParser `dmplus` kParser where
+        -- S combinator
+        sParser :: Parser Term
+        sParser = do
+            _ <- symb "S"
+            return S
+        
+        -- K combinator    
+        kParser :: Parser Term
+        kParser = do
+            _ <- symb "K"
+            return K
+    
+    subtermParser :: Parser Term
+    subtermParser = termParser' `dmplus` combinatorParser where
+        termParser' :: Parser Term
+        termParser' = do
+            _ <- symb "("
+            t <- termParser
+            _ <- symb ")"
+            return t
+            
+    termParser :: Parser Term
+    termParser = subtermsParser where
+        subtermsParser :: Parser Term
+        subtermsParser = do
+            ts <- pstar subtermParser
+            return $ apply' (head ts) (tail ts) where
+                apply' :: Term -> [Term] -> Term
+                apply' t [] = t
+                apply' t (t':[]) = App t t'
+                apply' t (t':ts) = apply' (App t t') ts
+                    
+    parseCL :: String -> Maybe Term
+    parseCL s = case apply termParser s of
+        (x:_) -> if null (snd x) then
+             Just $ fst x else Nothing
+        _ -> Nothing
---
+++ src/ParserUtils.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE DoAndIfThenElse #-}
+{-|
+    Copyright (c) 2014 Maciej Bendkowski
+  
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+    
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-}
+module ParserUtils where
+    import Control.Monad
+    import Data.Char
+    
+    -- recursive descent parser type definition
+    newtype Parser a = Parser (String -> [(a, String)])
+    parse :: Parser a -> String -> [(a, String)]
+    parse (Parser p) = p
+    
+    instance Monad Parser where
+        return x = Parser $ \s -> [(x, s)]
+        p >>= f = Parser $ \s -> concat [ parse (f x) s' | (x, s') <- parse p s]
+        
+    instance MonadPlus Parser where
+        mzero = Parser $ const []
+        p `mplus` q = Parser $ \s -> parse p s ++ parse q s
+        
+    -- deterministic choice argument
+    dmplus :: Parser a -> Parser a -> Parser a
+    p `dmplus` q = Parser $ \s -> case parse (p `mplus` q) s of
+        (x:_) -> [x]
+        _ -> []
+    
+    -- single char parser    
+    item :: Parser Char
+    item = Parser $ \s -> case s of
+        x:xs -> [(x, xs)]
+        _ -> []
+    
+    -- single char predicate parser    
+    itemPred :: (Char -> Bool) -> Parser Char
+    itemPred p = do
+        x <- item
+        if p x then return x
+        else mzero
+    
+    -- single char parser functional  
+    char :: Char -> Parser Char
+    char x = itemPred (x ==)
+    
+    -- single string parser functional
+    str :: String -> Parser String
+    str "" = return ""
+    str (x:xs) = do
+        _ <- char x
+        _ <- str xs
+        return (x:xs)
+        
+    -- Kleene star operator    
+    star :: Parser a -> Parser [a]
+    star p = pstar p `dmplus` return [] 
+    
+    -- Kleene plus operator
+    pstar :: Parser a -> Parser [a]
+    pstar p = do
+        x <- p
+        xs <- star p
+        return (x:xs)
+        
+    -- whitespace parser
+    space :: Parser String
+    space = star $ itemPred isSpace
+    
+    -- parse a token and then remove trailing space
+    token :: Parser a -> Parser a
+    token p = do
+        x <- p
+        _ <- space
+        return x
+    
+    -- parse a symbolic token   
+    symb :: String -> Parser String
+    symb s = token $ str s
+    
+    -- apply a parser removing leading space
+    apply :: Parser a -> String -> [(a, String)]
+    apply p = parse (do { _ <- space; p })
